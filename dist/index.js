346e3aa13c69bc1a6c8fd6c9e4656fc962464656
Chanhyeok Pakr <qkrcksgur999@gmail.com>
2024-06-09 17:06:05 +0900

    create classes to apply sperated logic

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,19 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
-const matches = fs_1.default
-    .readFileSync("./football.csv", {
-    encoding: "utf-8",
-})
-    .split("\n")
-    .map((row) => {
-    return row.split(",");
-});
+const CsvFileReader_1 = require("./CsvFileReader");
+const MatchResult_1 = require("./MatchResult");
+// 데이터 소스가 하드코딩 되어있음 => 데이터 추출을 재사용가능하게 할 수 있는 방법은?
+// csvFileReader class를 만들어서 해결
+// 문제3: 모든 데이터가 문자열로 파싱됨 => 각 속성에 맞게 데이터를 바꾸어 주어야함
+const reader = new CsvFileReader_1.CsvFileReader("./football.csv");
+reader.read();
+// "H","A"가 하드코딩되어있음 - emun을 이용
 let manUnitiedWins = 0;
-for (let match of matches) {
-    if (match[1] === "Man United" && match[5] === "H") {
+for (let match of reader.data) {
+    if (match[1] === "Man United" && match[5] === MatchResult_1.MatchResult.HomeWin) {
         manUnitiedWins++;
     }
-    else if (match[2] === "Man United" && match[5] === "A") {
+    else if (match[2] === "Man United" && match[5] === MatchResult_1.MatchResult.AwayWin) {
         manUnitiedWins++;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
-const matches = fs
-  .readFileSync("./football.csv", {
-    encoding: "utf-8",
-  })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
+
+// 데이터 소스가 하드코딩 되어있음 => 데이터 추출을 재사용가능하게 할 수 있는 방법은?
+// csvFileReader class를 만들어서 해결
+
+// 문제3: 모든 데이터가 문자열로 파싱됨 => 각 속성에 맞게 데이터를 바꾸어 주어야함
+const reader = new CsvFileReader("./football.csv") 
+reader.read();
+
+// "H","A"가 하드코딩되어있음 - emun을 이용
+
+
 
 let manUnitiedWins = 0;
 
-for (let match of matches) {
-  if (match[1] === "Man United" && match[5] === "H") {
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitiedWins++;
-  } else if (match[2] === "Man United" && match[5] === "A") {
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
     manUnitiedWins++;
   }
 }
